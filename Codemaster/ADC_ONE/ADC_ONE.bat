9fa5f22e9c81b259ffefca6aa055d4ee61777fe9
Дмитрий <Demenevdmitriy22@gmail.com>
2017-02-25 23:37:31 +0300

    All comments were deleted

---
+++ Codemaster/ADC_ONE/ADC_ONE.bat
@@ -0,0 +1,10 @@
+REM   C:\Users\Дмитрий\Desktop\Дима\4 курс\2 семестр\Курсовая работа 4курс 2 сем\Codemaster\ADC_ONE\ADC_ONE.ipp - command file
+REM   Created: Feb 25, 2017  23:24:38
+
+PushD
+CD..
+If Exist "C:\Users\Дмитрий\Desktop\Дима\4 курс\2 семестр\Курсовая работа 4курс 2 сем\Codemaster\ADC_ONE\Debug\Exe\ADC_ONE.MCE" Del "C:\Users\Дмитрий\Desktop\Дима\4 курс\2 семестр\Курсовая работа 4курс 2 сем\Codemaster\ADC_ONE\Debug\Exe\ADC_ONE.MCE"
+If Exist "C:\Users\Дмитрий\Desktop\Дима\4 курс\2 семестр\Курсовая работа 4курс 2 сем\Codemaster\ADC_ONE\Debug\Exe\ADC_ONE.HEX" Del "C:\Users\Дмитрий\Desktop\Дима\4 курс\2 семестр\Курсовая работа 4курс 2 сем\Codemaster\ADC_ONE\Debug\Exe\ADC_ONE.HEX"
+"D:\CodeMaster-96\1_32_00\CMC-96\Bin\MCC96.EXE" @"C:\Users\Дмитрий\Desktop\Дима\4 курс\2 семестр\Курсовая работа 4курс 2 сем\Codemaster\ADC_ONE\ADC_ONE.{1}"
+"D:\CodeMaster-96\1_32_00\CMC-96\Bin\MCLINK.EXE" @"C:\Users\Дмитрий\Desktop\Дима\4 курс\2 семестр\Курсовая работа 4курс 2 сем\Codemaster\ADC_ONE\ADC_ONE.(1)"
+PopD
